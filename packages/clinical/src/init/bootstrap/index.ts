591b2b7faae3ebbf83f9863557a82147acc85695
Derived <ytdli08@gmail.com>
2026-08-03 18:49:57 -0400

    refactor: remove deprecated clinical bootstrap initialization logic and record normalizers

--- packages/clinical/src/init/bootstrap/index.ts
+++
@@ -1,6 +0,0 @@
-// export { bootstrapClinicalStores } from "./bootstrap-writer";
-export type { BootstrapStores } from "./stores";
-
-// TODO(v2-bootstrap): the V2 builder/runtime owns initialization. These
-// retained store interfaces only cover non-parser reference data while the
-// old seed orchestrator is removed.
--- packages/clinical/src/init/bootstrap/normalizers/index.ts
+++
@@ -1,5 +0,0 @@
-export { normalizeConceptRelation } from "./concept-relation";
-export { normalizeFacility } from "./facility";
-export { normalizeJurisdictionalDisplay } from "./jurisdictional-display";
-export { normalizePersonnel } from "./personnel";
-export { normalizeProseRule } from "./prose-rule";
